07dff8442ff2f8ceeb6ed15116357b58075b26ad
edisonchachalo <edison.chachalo@udla.edu.ec>
2025-04-10 00:36:13 -0500

    Avance1

--- S3_Taller_ChachaloEdison.c
+++ S3_Taller_ChachaloEdison.c
@@ -4,12 +4,12 @@
 int main(int argc, char *argv[])
 {
 
-    char materia[5][20];
+    char materia[3][20];
     char estudiante[5][30];
-    char id[5][15];
-    float n1[5], n2[5], n3[5];
-    float suma, promedio;
-    int opc, contM = 0, contE = 0, mat = 0, len, aux;
+    float calificaciones[5][3];
+    float promedio = 0, max = 0, min = 10, nota;
+    int opc, contM = 0, contE = 0, auxM, auxE, len;
+    int aprobados = 0, reprobados = 0, maxM = 0, minM = 10;
 
     do
     {
@@ -18,8 +18,7 @@ int main(int argc, char *argv[])
         printf("2. Ingresar Notas\n");
         printf("3. Informacion Estudiantes\n");
         printf("4. Informacion Materias\n");
-        printf("5. Informacion del Curso\n");
-        printf("6. Salir\n");
+        printf("5. Salir\n");
         printf(">> ");
         scanf("%d", &opc);
 
@@ -29,7 +28,7 @@ int main(int argc, char *argv[])
             do
             {
 
-                printf("Ingrese el nombre de la materia m%d: ", contM);
+                printf("Ingrese el nombre de la materia %d: ", contM);
                 fflush(stdin);
                 fgets(materia[contM], 30, stdin);
 
@@ -41,50 +40,132 @@ int main(int argc, char *argv[])
 
             break;
         case 2:
+            if (contM == 0)
+            {
+                printf("No hay materias registradas. Primero registre\n");
+                break;
+            }
+
             printf("Materias Registradas\n");
             printf("#\t\tMateria\n");
             for (int i = 0; i < contM; i++)
             {
                 printf("%d\t\t%s\n", i, materia[i]);
             }
-            printf("Ingreso de Notas\n");
+            printf("\nIngreso de Notas\n");
             do
             {
-                printf("Ingrese el id del estudiate %d: ", contE);
-                scanf("%s", id[contE]);
+
                 printf("Ingrese el nombre del estudiante %d: ", contE);
                 fflush(stdin);
                 fgets(estudiante[contE], 30, stdin);
 
-                len = strlen(materia[contE]) - 1;
+                len = strlen(estudiante[contE]) - 1;
                 estudiante[contE][len] = '\0';
 
-                printf("Ingrese la Nota de la materia 0: ");
-                scanf("%f", &n1);
-                printf("Ingrese la Nota de la materia 1: ");
-                scanf("%f",&n2);
-                printf("Ingrese la Nota de la materia 2: ");
-                scanf("%f",&n3);
+                printf("Ingrese las Notas de %s:\n", estudiante[contE]);
+                float suma = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    do
+                    {
+                        printf("%s: ", materia[i]);
+                        scanf("%f", &calificaciones[contE][i]);
+                        if (calificaciones[contE][i] < 0 || calificaciones[contE][i] > 10)
+                        {
+                            printf("Nota no valida. La calificacion debe estar entre 0 y 10\n");
+                        }
 
-                mat++;
-
+                    } while (calificaciones[contE][i] < 0 || calificaciones[contE][i] > 10);
+                    suma += calificaciones[contE][i];
+                }
+                promedio = suma / 3;
+                printf("Promedio: %.2f\n", promedio);
                 contE++;
             } while (contE < 5);
 
             break;
         case 3:
+            if (contE == 0)
+            {
+                printf("No hay estudiantes registrados\n");
+                break;
+            }
+
+            printf("Lista de estudiantes\n");
+            printf("#\tNombre\n");
+            for (int i = 0; i < contE; i++)
+            {
+                printf("%d\t%s\n", i, estudiante[i]);
+            }
+            printf("\nSeleccione un estudiante\n");
+            printf("Ingrese el # del estudiante en la lista: ");
+            scanf("%d", &auxE);
+            if (auxE < 0 || auxE >= contE)
+            {
+                printf("Estudiante no se encuentra en la lista");
+                break;
+            }
+
+            printf("\nPromedio de %s: %.2f\n", estudiante[auxE], promedio);
+            printf("Nota mas alta: %.2f\n", max);
+            printf("Nota mas baja: %.2f\n", min);
 
             break;
         case 4:
+            if (contM == 0)
+            {
+                printf("No hay materias registradas\n");
+                break;
+            }
+            printf("Lista de Materias\n");
+            printf("#\tNombre\n");
+            for (int i = 0; i < contM; i++)
+            {
+                printf("%d\t%s\n", i, materia[i]);
+            }
+            printf("Seleccione una Materia\n");
+            printf("Ingrese el # de la Materia de la lista: ");
+            scanf("%d", auxM);
+            if (auxM < 0 || auxM > contM)
+            {
+                printf("# ingresado no se encuentra en la lista");
+                break;
+            }
+            for (int i = 0; i < contE; i++)
+            {
+                nota = calificaciones[i][auxM];
+                if (nota >= 6)
+                {
+                    aprobados++;
+                }
+                else
+                {
+                    reprobados++;
+                }
+                if (nota > maxM)
+                {
+                    maxM = nota;
+                }
+                if (nota < minM)
+                {
+                    minM = nota;
+                }
+            }
+            printf("\nMateria: %s\n", materia[auxM]);
+            printf("Estudiantes aprobados: %d\n", aprobados);
+            printf("Estudiantes Reprobados: %d\n", reprobados);
+            printf("Nota mas alta en la Materia: %.2f\n", maxM);
+            printf("Nota mas baja en la Materia: %.2f\n", minM);
 
             break;
         case 5:
-
+            printf("Saliendo del sistema ... \n");
             break;
         default:
             break;
         }
-    } while (opc != 6);
+    } while (opc != 5);
 
     return 0;
 }
